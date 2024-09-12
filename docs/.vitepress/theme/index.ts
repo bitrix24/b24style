6a1161885366eb635bb25b2afb2f7a59802f6101
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-09-12 10:15:09 +0300

    add colors, spacing fix code style

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,16 +3,17 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import DynamicLayout from '../components/DynamicLayout.vue'
 import ShTailwindCssLink from '../components/ShTailwindCssLink.vue'
-import ShDocTableInfo from '../components/ShDocTableInfo.vue'
+// import ShDocTableInfo from '../components/ShDocTableInfo.vue'
 import ColorPaletteReference from '../components/ColorPaletteReference.vue'
 import ColorPaletteReferenceGroup from '../components/ColorPaletteReferenceGroup.vue'
+import SpacingScale from '../components/SpacingScale.vue'
 
 import './style.css'
 
 import presetBorder from '../../../plugins/preset-border';
 import presetColors from '../../../plugins/preset-colors';
 import presetFonts from '../../../plugins/preset-fonts';
-import groupsFonts from '../../../plugins/fonts';
+// import groupsFonts from '../../../plugins/fonts';
 
 import presetShadow from '../../../plugins/preset-shadow';
 import presetSize from '../../../plugins/preset-size';
@@ -26,16 +27,19 @@ export default {
 	Layout: DynamicLayout,
 	enhanceApp({app})
 	{
+		app.provide('presetColors', presetColors)
+		app.provide('presetSize', presetSize)
 		app.provide('presetText', presetText)
 		app.provide('presetBorder', presetBorder)
-		app.provide('presetColors', presetColors)
 		app.provide('presetFonts', presetFonts)
 		app.provide('presetShadow', presetShadow)
 		app.provide('presetAnimation', presetAnimation)
 		app.provide('presetTransition', presetTransition)
+		
 		app.component('ShTailwindCssLink', ShTailwindCssLink)
-		app.component('ShDocTableInfo', ShDocTableInfo)
+		//app.component('ShDocTableInfo', ShDocTableInfo)
 		app.component('ColorPaletteReference', ColorPaletteReference)
 		app.component('ColorPaletteReferenceGroup', ColorPaletteReferenceGroup)
+		app.component('SpacingScale', SpacingScale)
 	}
 } satisfies Theme
